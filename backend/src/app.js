af53cd5de93afda95d510284760224624544a980
sowmi1010 <tamilsowmi1010@gmail.com>
2025-09-30 11:17:23 +0530

    Prepare backend for Render

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ uploadDirs.forEach((dir) => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
